152f037c0f919e25a8bc18875d9fab4ec0b7e7e8
Marko <marko@Marko-MacBook-Pro.local>
2021-03-24 11:19:34 +0200

    changed names of scss files to capitalize

--- src/api/getWord.js
+++ src/api/getWord.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
-export const getWord = (word) => {
-  return axios.get(`http://localhost:8080/api/dictionary/search/${word}`).then((res) => res.data);
+export const getWord = async (word) => {
+  const res = await axios.get(`http://localhost:8080/api/dictionary/search/${word}`);
+  return res.data;
 };
