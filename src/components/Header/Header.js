d1a116b043ec1421fcede6aadfbbd8c04fe9e58c
Marko <marko@Marko-MacBook-Pro.local>
2021-04-11 23:06:53 +0300

    tables - done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,34 +5,15 @@ import 'antd/dist/antd.css';
 
 import { Layout, Menu, Breadcrumb } from 'antd';
 
-// import s from './header.module.scss';
+import s from './header.module.scss';
 
 const { Header, Content, Footer } = Layout;
 
 export const HeaderPage = () => {
   return (
     <Layout>
-      <Header
-        style={{
-          background: 'linear-gradient(rgb(0, 21, 41) 40%, rgb(53, 50, 50) 100%)',
-          padding: '5px 0 5px 40px',
-          height: '70px',
-        }}
-      >
-        <span
-          style={{
-            color: 'white',
-            fontSize: '30px',
-            fontWeight: 'bold',
-            height: '40px',
-            borderBottom: '1px solid orange',
-            borderTop: '1px solid orange',
-            padding: '6px',
-            borderRadius: '5px',
-          }}
-        >
-          S&ensp; S&ensp;L&ensp;U&ensp;W
-        </span>
+      <Header className={s.header}>
+        <span className={s.header__logo}>S&ensp; S&ensp;L&ensp;U&ensp;W</span>
       </Header>
     </Layout>
   );
