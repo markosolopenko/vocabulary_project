2279eb271ea01e3f3ab76c156984c3faeeba12ac
Marko <marko@Marko-MacBook-Pro.local>
2021-04-12 10:48:58 +0300

    subscription - styled subscription component and make tabs fixed and body of tab sudscription will scroll

--- src/components/SubscriptionForWord/Subscription.js
+++ src/components/SubscriptionForWord/Subscription.js
@@ -1,3 +1,7 @@
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
 import s from './Subscription.module.scss';
 
 export const Subscription = ({ wordJson }) => {
@@ -5,21 +9,23 @@ export const Subscription = ({ wordJson }) => {
     <div className={s.subscription}>
       {Object.keys(wordJson).length !== 0 && wordJson.definitions.length !== 0 ? (
         wordJson.definitions.map((definition, id) => (
-          <div className={s.subscription__item}>
-            <div className={s.subscription__item__definition} key={id}>
-              {definition.definition}
-            </div>
-            {definition.meanings &&
-              definition.meanings.map((meaning, index) => (
-                <p className={s.subscription__item__meaning} style={{ width: '100vh' }} key={index}>
-                  {meaning.meaning}
-                </p>
-              ))}
-            )
-          </div>
+          <Card className={s.subscription__root}>
+            <CardContent>
+              <Typography variant="h7" component="h3" key={id}>
+                {definition.definition}
+              </Typography>
+              {definition.meanings &&
+                definition.meanings.map((meaning, index) => (
+                  <Typography variant="body2" component="p" key={index}>
+                    {meaning.meaning}
+                  </Typography>
+                ))}
+              )
+            </CardContent>
+          </Card>
         ))
       ) : (
-        <div>Nothing found</div>
+        <div className={s['not-found']}>Для цього слова немає опису</div>
       )}
     </div>
   );
